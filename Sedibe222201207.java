39a74f6c5c555a8c31e1c549d1b28a5e4986db5b
Student <student@example.com>
2025-05-12 11:31:23 +0200

    Updated Sedibe222201207.java with additional text lines

--- Sedibe222201207.java
+++ Sedibe222201207.java
@@ -1,5 +1,7 @@
 public class Sedibe222201207 {
     public static void main(String[] args) {
         System.out.println("Hello, this is a test output!");
+        System.out.println("Now we have more text lines.");
+        System.out.println("This is an updated version of the file.");
     }
 }
